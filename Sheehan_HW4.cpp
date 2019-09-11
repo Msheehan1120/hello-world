1ff9145981f9d680b15d7ab1a468eb03f053880e
Michaelena Sheehan <Msheehan1120@gmail.com>
2019-09-10 21:54:29 -0500

    Update Sheehan_HW4.cpp

---
+++ Sheehan_HW4.cpp
@@ -0,0 +1,16 @@
+/*
+ Program name: small World 
+ Purpose: Extend program functionality and experience
+ Author: [Michaelena Sheehan]
+ Last Date modified [9/10/19]
+ */
+#include <iostream> 
+
+using namespace std;
+ 
+int main ()
+{
+    cout <<"Hello World Michaelena Sheehan"<<endl;
+    return 0;
+}
+
