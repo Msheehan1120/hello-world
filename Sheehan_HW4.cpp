3dcab7595822e4423f092a31ce31de1f48710112
Michaelena Sheehan <Msheehan1120@gmail.com>
2019-09-10 22:17:39 -0500

    comment  2

--- Sheehan_HW4.cpp
+++ Sheehan_HW4.cpp
@@ -1,8 +1,8 @@
+// Program name: small World 
+// Purpose: Extend program functionality and experience
+// Author: [Michaelena Sheehan]
+// Last Date modified [9/10/19]
 
-//Program name: small World 
-//Purpose: Extend program functionality and experience
-//Author: [Michaelena Sheehan]
-//Last Date modified [9/10/19]
 #include <iostream>  
 
 using namespace std;
